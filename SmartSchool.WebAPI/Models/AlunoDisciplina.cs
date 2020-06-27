f5876862b3cf89dbe5200bc0bb392ae3675350a6
huggosantos <huggo.oliveira1@gmail.com>
2020-06-27 14:12:22 -0300

    Configuração SQLite + Migrations + input de dados

--- SmartSchool.WebAPI/Models/AlunoDisciplina.cs
+++ SmartSchool.WebAPI/Models/AlunoDisciplina.cs
@@ -2,6 +2,9 @@ namespace SmartSchool.WebAPI.Models
 {
     public class AlunoDisciplina
     {
+        public AlunoDisciplina()
+        {            
+        }
         public AlunoDisciplina(int alunoId, int disciplinaId)
         {
             this.AlunoId = alunoId;          
